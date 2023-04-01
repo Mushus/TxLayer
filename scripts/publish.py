da5694b9820ed67de65b58672573feda0c3f5f29
Mushus <6138982+Mushus@users.noreply.github.com>
2023-04-01 11:31:55 +0900

    fix print version

--- scripts/publish.py
+++ scripts/publish.py
@@ -1,15 +1,14 @@
 import sys, os, re
 
-version = sys.argv[1]
+version = sys.argv[1] if len(sys.argv) > 1 else None
 
+target = -1
 if version == 'patch':
     target = 2
 elif version == 'minor':
     target = 1
 elif version == 'major':
     target = 0
-else:
-    raise Exception('Invalid version')
 
 pattern = re.compile(r'"version"\s*:\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)')
 
@@ -18,13 +17,21 @@ def replace_version(match):
     v[target] += 1
     return '"version": (%d, %d, %d)' % tuple(v)
 
+def print_version(content):
+    match = pattern.search(content)
+    if match:
+        print('.'.join(match.groups()))
+
 edit_target_path = os.path.join(os.path.dirname(__file__), '..', '__init__.py')
 encoding = "utf-8"
 
 with open(edit_target_path, 'r', encoding=encoding) as reader:
     content = reader.read()
 
-content = pattern.sub(replace_version, content)
+if target != -1:
+    content = pattern.sub(replace_version, content)
+
+print_version(content)
 
 with open(edit_target_path, 'w', encoding=encoding) as writer:
     writer.write(content)
